1a46e9b96aa6171a4ed1a95a101185da048833e1
gkswns103 <gkswns103@naver.com>
2023-05-24 10:08:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myphonesensor/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myphonesensor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tvSensor = findViewById(R.id.tvSensor);
+        SensorManager sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sm.getSensorList(Sensor.TYPE_ALL);
+
+        String sensorInfo = "센서의 개수: " + sensorList.size() + "\n\n";
+
+        int i = 1;
+        for(Sensor s : sensorList) {
+            sensorInfo = sensorInfo + i++ + "\nName : " + s.getName() + "\n해상도 : " +s.getResolution()
+                    + "\n최대범위 : " + s.getMaximumRange() + "\n\n";
+        }
+
+        tvSensor.setText(sensorInfo);
+    }
+}
